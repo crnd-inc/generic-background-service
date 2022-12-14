1ca21336b63738f4c328e94f44396d123f222772
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-12-14 23:55:39 +0200

    [13.0] Initial port to 13.0

--- test_generic_background_service/__manifest__.py
+++ test_generic_background_service/__manifest__.py
@@ -7,7 +7,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Hidden',
-    'version': '12.0.0.1.0',
+    'version': '13.0.0.1.0',
     'depends': [
         'generic_mixin',
         'generic_background_service',
