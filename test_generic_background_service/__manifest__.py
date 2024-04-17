377d22d37d2b879cc09d4d37e09b39c02657bca6
Vadim-23 <vadim.mudruk@gmail.com>
2024-04-17 11:59:18 +0300

    [FIX] fix for fwp to 17 version
    Signed-off-by: Vadim-23 <vadim.mudruk@gmail.com>

--- test_generic_background_service/__manifest__.py
+++ test_generic_background_service/__manifest__.py
@@ -7,7 +7,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Hidden',
-    'version': '16.0.0.1.0',
+    'version': '17.0.0.1.0',
     'depends': [
         'generic_mixin',
         'generic_background_service',
