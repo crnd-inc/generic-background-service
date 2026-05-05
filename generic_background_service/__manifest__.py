6557c34f8dbd8b724086d2ccc2ccaac31233c56e
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-05-05 14:11:12 +0300

    Bump versions

--- generic_background_service/__manifest__.py
+++ generic_background_service/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Generic Background Service",
-    "version": "18.0.0.2.1",
+    "version": "18.0.0.2.2",
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     "license": "LGPL-3",
