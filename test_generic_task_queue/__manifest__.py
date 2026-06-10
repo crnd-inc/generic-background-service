7ea936b43a1b493cbac64908abdd2555513dfec7
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-06-10 11:56:37 +0300

    Bump versions

--- test_generic_task_queue/__manifest__.py
+++ test_generic_task_queue/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Hidden',
-    'version': '18.0.0.0.8',
+    'version': '18.0.0.0.9',
     'depends': [
         'generic_task_queue',
     ],
